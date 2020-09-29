6c685e775ccbc7e09539eb27accd0809704473b0
sfahim-amphealth <sfahim@adaptivemedicalpartners.com>
2020-09-29 09:35:52 -0500

    Finished writing and refactoring palindrome method

--- lib/slrfhm_palindrome.rb
+++ lib/slrfhm_palindrome.rb
@@ -1,6 +1,17 @@
 require "slrfhm_palindrome/version"
 
-module SlrfhmPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  private
+
+  # Returns content for palindrome testing.
+  def processed_content
+    self.scan(/[a-z]/i).join.downcase
+  end
+
 end
--- test/slrfhm_palindrome_test.rb
+++ test/slrfhm_palindrome_test.rb
@@ -6,6 +6,25 @@ class SlrfhmPalindromeTest < Minitest::Test
   end
 
   def test_it_does_something_useful
-    assert false
+    assert true
   end
+
+  def test_non_palindrome
+    refute "apple".palindrome?
+  end
+
+  def test_literal_palindrome
+    assert "racecar".palindrome?
+  end
+
+  def test_mixed_case_palindrome
+    assert "Madam, I'm Adam.".palindrome?
+  end
+
+
+  def test_palindrome_with_punctuation
+    assert "A man, a plan, a canal - Panama!".palindrome?
+  end
+
 end
+
